36e73df0061595ccd8584db01413870f0aa2b27d
Calvin Williamson <calvin.e.williamson@gmail.com>
2019-09-04 15:22:47 -0400

    install.R and rutime move to ipynb dir

--- ipynb/install.R
+++ ipynb/install.R
@@ -1,5 +1,4 @@
-#r = getOption("repos") 
-#r["CRAN"] = "https://cloud.r-project.org"
-#options(repos = r)
-#rm(r)
-
+r = getOption("repos") 
+r["CRAN"] = "https://cloud.r-project.org"
+options(repos = r)
+rm(r)
