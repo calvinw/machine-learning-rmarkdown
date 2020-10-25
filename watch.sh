6aac2e62476859fc3aa4f6c6710b7f230b91de76
Calvin Williamson <calvin.e.williamson@gmail.com>
2020-10-25 00:30:40 -0400

    adding accuracy and two products

--- watch.sh
+++ watch.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-python -m SimpleHTTPServer 8000 &>/dev/null &
+#python -m SimpleHTTPServer 8000 &>/dev/null &
+Rscript -e "servr::httw(pattern='*.html')" &
 
 pid=$!
 
 trap ctrl_c EXIT 
 function ctrl_c() {
+    echo "Killing the servr::httw"
     kill "${pid}"
 }
 
+echo "calling ls *.Rmd | entr make"
 ls *.Rmd | entr make
